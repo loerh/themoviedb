7b4e42027ce95eff2c7ecf2c0f5959b1c5850988
laurentmeert <laurent_meert@yahoo.com>
2018-04-30 10:22:26 +0200

    Initial Commit

---
+++ themoviedbUITests/themoviedbUITests.swift
@@ -0,0 +1,36 @@
+//
+//  themoviedbUITests.swift
+//  themoviedbUITests
+//
+//  Created by Laurent Meert on 30/04/2018.
+//  Copyright © 2018 Laurent Meert. All rights reserved.
+//
+
+import XCTest
+
+class themoviedbUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
