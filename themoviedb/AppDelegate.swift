42c9e03bd3c76f04b7d9cc5d6ccc51b711842229
laurentmeert <laurent_meert@yahoo.com>
2018-05-04 12:14:01 +0200

    search bar appearance

--- themoviedb/AppDelegate.swift
+++ themoviedb/AppDelegate.swift
@@ -14,7 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        /// Search bar appearance
+        if let font =  UIFont(name: "HelveticaNeue", size: 14) {
+            UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).font = font
+            UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).setTitleTextAttributes([.font: font.withSize(15)], for: .normal)
+        }
+        
+        
         return true
     }
 }
